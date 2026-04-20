f01637ff84006b1fc170417be5b143f8b4fead2a
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-20 19:35:11 -0300

    commit 2.1

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ int main(){
     std::string nomes[3] = {"João", "Paula", "Murilo"};
     int idades[3] = {27, 31, 20};
     
+    std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos."<<std::endl;
+    
     return 0;
 }
