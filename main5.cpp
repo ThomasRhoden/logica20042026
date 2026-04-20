0478180e605da375188c006a2e9266c0527ebaee
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-20 20:59:02 -0300

    commit pos intervalo

---
+++ main5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+int main(){
+
+    std::string letras[2][4] = {
+        {"A", "B", "C", "D"},
+        {"E", "F", "G", "H"}
+    };
+
+    std::cout<<letras[1][0]<<std::endl;
+
+    return 0;
+}
