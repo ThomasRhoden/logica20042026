909f157d9b37e21143b013280df1a4be9e1677de
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-20 19:47:43 -0300

    commit 3x

---
+++ main2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+int main(){
+
+    int numeros[5] = {10, 20, 30, 40, 50};
+
+    int tamanho = std::size(numeros);
+
+    for(int i = 0; i < tamanho; i++){
+        std::cout<<numeros[i]<<std::endl;
+    }
+
+
+    return 0;
+}
