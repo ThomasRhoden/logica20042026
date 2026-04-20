a31dd95879c833a6b50609c5cbd9c839514e3676
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-20 19:57:37 -0300

    commit quarto

---
+++ main3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+int main(){
+
+    std::string carros[5] = {"Volvo", "BMW", "Ford", "Chevrolet", "Fiat"};
+
+    for(int i = 0; i < 5; i++){
+        std::cout<<carros[i]<<std::endl;
+    }
+
+
+
+    return 0;
+}
