6afb485c4070d24e7a0e017afbdf85bbc04ae536
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-20 19:43:37 -0300

    commit proprio 1xx

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@
 
 int main(){
 
-    std::string nomes[3] = {"João", "Paula", "Murilo"};
+    std::string nomes[3] = {"Joao", "Paula", "Murilo"};
     int idades[3] = {27, 31, 20};
-    
-    std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos."<<std::endl;
+    int x, y, z;
+
+
+    std::cout<<"Digite 0, enter, 1, enter, 2, enter!"<<std::endl;
+
+    std::cin>>x;
+    std::cin>>y;
+    std::cin>>z;
+
+    std::cout<<nomes[x]<<" tem "<<idades[x]<<" anos."<<std::endl;
+    std::cout<<nomes[y]<<" tem "<<idades[y]<<" anos."<<std::endl;
+    std::cout<<nomes[z]<<" tem "<<idades[z]<<" anos."<<std::endl;
     
     return 0;
 }
