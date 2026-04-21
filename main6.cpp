36107e0fcbfcd8868c364c5cf30f9ab85dfc8e3e
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-20 21:09:22 -0300

    commit 6

---
+++ main6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+
+    int contagem = 10;
+
+    while(contagem > 0){
+        std::cout<<contagem<<std::endl;
+        contagem--;
+    }
+
+    std::cout<<"Feliz ano novo!!"<<std::endl;
+
+    return 0;
+}
