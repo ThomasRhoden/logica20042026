f8a144a60366c64582156765cf1decef9344533e
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-20 21:42:25 -0300

    oitavo comit

---
+++ main8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main(){
+
+    int num;
+
+    do{
+        std::cout<<"Digite um valor para num: "<<std::endl;
+        std::cin>>num;
+
+        if(num < 1 || num > 10){
+            std::cout<<"Entrada invalida!!"<<std::endl;
+        }
+    }while(num < 1 || num > 10);{
+        std::cout<<"Obrigado!! Voce escolheu o numero: "<<num<<std::endl;
+    }
+
+
+    return 0;
+}
